d613ce6342942f5a5cef5ac33110d31052bacf4c
Adria Martínez <adriamartinez94@gmail.com>
2022-03-19 11:59:46 +0100

    Recycler view en home fragment

--- app/src/main/java/com/example/meetify/view/HomeFragment.kt
+++ app/src/main/java/com/example/meetify/view/HomeFragment.kt
@@ -7,17 +7,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.meetify.R
 import com.example.meetify.databinding.HomeFragmentBinding
+import com.example.meetify.model.MeetProvider
+import com.example.meetify.view.adapters.MeetAdapter
 
 class HomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
-
     private lateinit var viewModel: HomeViewModel
     private lateinit var binding: HomeFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +27,16 @@ class HomeFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var recyclerView = binding.recyclerViewMeets
+        val adapter = MeetAdapter(MeetProvider.getMeets())
+
+        recyclerView.layoutManager = LinearLayoutManager(this.context)
+        recyclerView.adapter = adapter
+
+    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
--- app/src/main/java/com/example/meetify/view/adapters/MeetAdapter.kt
+++ app/src/main/java/com/example/meetify/view/adapters/MeetAdapter.kt
@@ -1,21 +1,39 @@
 package com.example.meetify.view.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.meetify.R
+import com.example.meetify.databinding.ItemMeetBinding
 import com.example.meetify.model.MeetModel
 
 class MeetAdapter(val meetList:List<MeetModel>):RecyclerView.Adapter<MeetAdapter.MeetHolder>() {
+
     inner class MeetHolder(val view: View):RecyclerView.ViewHolder(view) {
+        var binding: ItemMeetBinding = ItemMeetBinding.inflate(LayoutInflater.from(view.context))
+        fun render(meet:MeetModel){
+            view.findViewById<TextView>(R.id.tvtitle).text = meet.name
+            view.findViewById<TextView>(R.id.tv_people_count).text = meet.people.toString()
+            view.findViewById<TextView>(R.id.tv_hour).text = meet.hour.toString() + ":00"
+
+            view.findViewById<Button>(R.id.btn_join_meet).setOnClickListener {
+
+            }
+
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MeetHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return MeetHolder(layoutInflater.inflate(R.layout.item_meet, parent, false))
     }
 
     override fun onBindViewHolder(holder: MeetHolder, position: Int){
-
+        holder.render(meetList[position])
     }
 
     override fun getItemCount(): Int {
